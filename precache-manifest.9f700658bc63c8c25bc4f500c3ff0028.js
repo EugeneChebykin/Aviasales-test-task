1b846b9d57c3f08690e49e0f0d43de350d3041d7
EugeneChebykin <respublicofrome@gmail.com>
2020-01-06 14:56:50 +0300

    Updates

---
+++ precache-manifest.9f700658bc63c8c25bc4f500c3ff0028.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2073ca01729e9ad34fa4b33ee140d224",
+    "url": "/Aviasales-test-task/index.html"
+  },
+  {
+    "revision": "8c5612c752187aa0db76",
+    "url": "/Aviasales-test-task/static/css/main.fc33148e.chunk.css"
+  },
+  {
+    "revision": "cf243d8bf1e2eb0d4501",
+    "url": "/Aviasales-test-task/static/js/2.1c5acb66.chunk.js"
+  },
+  {
+    "revision": "095866c06919ebe24bd7a70c281ea4b9",
+    "url": "/Aviasales-test-task/static/js/2.1c5acb66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c5612c752187aa0db76",
+    "url": "/Aviasales-test-task/static/js/main.cc0f1a11.chunk.js"
+  },
+  {
+    "revision": "787639e7cc138736289b",
+    "url": "/Aviasales-test-task/static/js/runtime-main.e651761b.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/Aviasales-test-task/static/media/logo.0c089234.svg"
+  }
+]);
